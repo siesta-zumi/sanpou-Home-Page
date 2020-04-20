b21ed8b4186ae5ee19bde05508f1e1663bb29e00
yasutaka <siesta27@gmail.com>
2020-04-20 17:00:55 +0900

    スクロールJSの修正

--- scroll.js
+++ scroll.js
@@ -1,58 +1,15 @@
 $(function() {
-  var home = $('#home_btn');
-  var about = $('#about_btn');
-  var works = $('#works_btn');
-  var items = $('#items_btn');
-  var news = $('#news_btn');
-  var recruit = $('#recruit_btn');
-  var contact = $('#contact_btn');
-
-  home.click(function () {
-    $('body,html').animate({
-        scrollTop: 0
-    }, 500);
-    return false;
-  });
-
-  about.click(function () {
-    $('body,html').animate({
-        scrollTop: 800
-    }, 500);
-    return false;
-  });
-
-  works.click(function () {
-    $('body,html').animate({
-        scrollTop: 1500
-    }, 500);
-    return false;
-  });
   
-  items.click(function () {
-    $('body,html').animate({
-        scrollTop: 2600
-    }, 500);
-    return false;
-  });
-
-  news.click(function () {
-    $('body,html').animate({
-        scrollTop: 3200
-    }, 500);
-    return false;
-  });
-  
-  recruit.click(function () {
-    $('body,html').animate({
-        scrollTop: 5300
-    }, 500);
-    return false;
-  });
-
-  contact.click(function () {
-    $('body,html').animate({
-        scrollTop: 5200
-    }, 500);
-    return false;
+  $('header a').click(function(){
+    
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html,body').animate({
+      'scrollTop': position
+    },500);
+    console.log(position);
   });
 });
+
+
+
