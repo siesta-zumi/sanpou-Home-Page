af448528b0d0e41ae28ef5610715fa90f331e3f1
yasutaka <siesta27@gmail.com>
2020-04-21 23:27:54 +0900

    hoverイベントの追加

--- scroll.js
+++ scroll.js
@@ -1,15 +1,22 @@
 $(function() {
-  
-  $('header a').click(function(){
-    
+  $('header a').click(function() {
     var id = $(this).attr('href');
     var position = $(id).offset().top;
-    $('html,body').animate({
-      'scrollTop': position
-    },500);
-    console.log(position);
+    $('html,body').animate({'scrollTop': position},800);
   });
-});
+}
+);
 
-
+$(function() {
+  $('.items li').hover(
+    function() {
+      
+    $(this).find(".item-info").addClass("active");
+    
+  },function() {
+    $(this).find(".item-info").removeClass("active");
+  }
+  );
+}
+);
 
